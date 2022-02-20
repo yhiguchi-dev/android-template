194302200c75688e300df89b7c4ba772c3d9f7e7
Yuki Higuchi <yuki@yhiguchi.dev>
2022-02-20 12:19:20 +0900

    add template

---
+++ app/src/main/java/dev/yhiguchi/template/MainActivity.kt
@@ -0,0 +1,52 @@
+package dev.yhiguchi.template
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.yhiguchi.template.ui.theme.TemplateTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      TemplateTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          color = MaterialTheme.colors.background
+        ) {
+          Greeting("Android")
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun Greeting(name: String) {
+  Text(text = "Hello $name!")
+}
+
+@Preview(
+  showBackground = true,
+  name = "Light Mode"
+)
+@Preview(
+  uiMode = Configuration.UI_MODE_NIGHT_YES,
+  showBackground = true,
+  name = "Dark Mode"
+)
+@Composable
+fun DefaultPreview() {
+  TemplateTheme {
+    Greeting("Android")
+  }
+}
