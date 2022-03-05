da4a1450c62342cfae79c40aac2353e2601cee8b
Yuki Higuchi <yuki@yhiguchi.dev>
2022-03-06 01:57:10 +0900

    update

--- app/src/test/java/dev/yhiguchi/template/ExampleUnitTest.kt
+++ app/src/test/java/dev/yhiguchi/template/ExampleUnitTest.kt
@@ -1,16 +1,10 @@
 package dev.yhiguchi.template
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
-  @Test
-  fun addition_isCorrect() {
-    assertEquals(4, 2 + 2)
+class ExampleUnitTest : StringSpec({
+  "sample test" {
+    2 + 2 shouldBe 4
   }
-}
+})
