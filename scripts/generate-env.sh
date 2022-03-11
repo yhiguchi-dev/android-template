eea6e37fb8d4a87d2af93429b742bd4ef2057b3d
Yuki Higuchi <yuki@yhiguchi.dev>
2022-03-12 01:25:49 +0900

    update

--- scripts/generate-env.sh
+++ scripts/generate-env.sh
@@ -4,8 +4,6 @@ set -eux
 
 cd $(dirname $0)/../
 
-mkdir -p config
-
 export BUILD_DATE=$(date "+%Y-%m-%d-%H-%M")
 
-envsubst < ./scripts/env.gradle.template > ./config/env.gradle
+envsubst < ./env.gradle.template > ./env.gradle
