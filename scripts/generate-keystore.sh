3ccf37e284b256010e31e0a5694f5e9fe4fa2d00
Yuki Higuchi <yuki@yhiguchi.dev>
2022-03-13 00:56:36 +0900

    update

--- scripts/generate-keystore.sh
+++ scripts/generate-keystore.sh
@@ -9,7 +9,8 @@ validity=${VALIDITY}
 storepass=${KEYSTORE_PASSWORD}
 alias=${KEY_ALIAS}
 dname=${DISTINGUISHED_NAME}
+outputDir=${KEYSTORE_OUTPUT_DIR}
 
-keytool -J-Dkeystore.pkcs12.legacy -genkey -v -keystore ./${keystore} -keyalg RSA -validity ${validity} -storepass ${storepass} -alias ${alias} -dname "${dname}"
+keytool -J-Dkeystore.pkcs12.legacy -genkey -v -keystore ${outputDir}/${keystore} -keyalg RSA -validity ${validity} -storepass ${storepass} -alias ${alias} -dname "${dname}"
 
-keytool -list -v -keystore ${keystore} -storepass ${storepass}
+keytool -list -v -keystore ${outputDir}/${keystore} -storepass ${storepass}
