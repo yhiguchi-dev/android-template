f9ac00a9d711f69f9d0863b150ca938e75363772
Yuki Higuchi <yuki@yhiguchi.dev>
2022-03-13 12:40:14 +0900

    update

--- scripts/generate-keystore.sh
+++ scripts/generate-keystore.sh
@@ -4,12 +4,23 @@ set -eux
 
 cd $(dirname $0)/../
 
-keystore=${KEYSTORE_FILE}
-validity=${VALIDITY}
-storepass=${KEYSTORE_PASSWORD}
-alias=${KEY_ALIAS}
-dname=${DISTINGUISHED_NAME}
-outputDir=${KEYSTORE_OUTPUT_DIR}
+debug=${DEBUG:=false}
+
+if "${debug}"; then
+  keystore=debug.keystore
+  validity=10950
+  storepass=android
+  alias=androiddebugkey
+  dname='CN=Android Debug, O=Android, C=US'
+  outputDir=./app
+else
+  keystore=${KEYSTORE_FILE}
+  validity=${VALIDITY}
+  storepass=${KEYSTORE_PASSWORD}
+  alias=${KEY_ALIAS}
+  dname=${DISTINGUISHED_NAME}
+  outputDir=${KEYSTORE_OUTPUT_DIR}
+fi
 
 keytool -J-Dkeystore.pkcs12.legacy -genkey -v -keystore ${outputDir}/${keystore} -keyalg RSA -validity ${validity} -storepass ${storepass} -alias ${alias} -dname "${dname}"
 
